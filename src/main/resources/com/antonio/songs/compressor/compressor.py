0e56717ca465a9c5479756fb4c6a7e4c3402b360
antoniotorresz <antoniiotorresz1@gmail.com>
2025-01-15 17:59:37 -0600

    song compressor

--- src/main/resources/com/antonio/songs/compressor/compressor.py
+++ src/main/resources/com/antonio/songs/compressor/compressor.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+#!./env/bin/python
 import os
 from pydub import AudioSegment
 
-def compress_song(input_file: str, output_file: str) -> None:
+def compress_song(input_file: str) -> None:
     song = AudioSegment.from_wav(input_file)
     song = song.set_frame_rate(16000).set_channels(1)
-    song.export(output_file, format="wav", bitrate="16k")
-    
-    for song in os.listdir():
+    song.export(input_file, format="wav", bitrate="16k")
+
+if __name__ == "__main__":
+    for song in os.listdir('..'):
         if song.endswith(".wav"):
-            compress_song(song, f"compressed_{song}")
-        compress_song(song, song)
+            compress_song(os.path.join('..', song))
